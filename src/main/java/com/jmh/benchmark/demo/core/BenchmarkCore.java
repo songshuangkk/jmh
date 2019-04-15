9dc523ca139de4b3af034ee266434d1d699640f6
songshuangkk <songshuangkk@gmail.com>
2019-04-15 13:31:07 +0800

    init test

---
+++ src/main/java/com/jmh/benchmark/demo/core/BenchmarkCore.java
@@ -0,0 +1,36 @@
+package com.jmh.benchmark.demo.core;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Thread)
+public class BenchmarkCore {
+
+  @Benchmark
+  public int sleepAWhile() {
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      // ignore
+    }
+    return 0;
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+        .include(BenchmarkCore.class.getSimpleName())
+        .forks(1)
+        .warmupIterations(5)
+        .measurementIterations(5)
+        .build();
+
+    new Runner(opt).run();
+  }
+}
