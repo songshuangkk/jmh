2c4c0191b98b296caa25620a9ae13df4fa952b10
songshuang <songshuang@babytree-inc.com>
2019-04-15 13:26:11 +0800

    first commit

---
+++ src/main/java/com/jmh/benchmark/demo/BenchmarkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jmh.benchmark.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmarkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchmarkDemoApplication.class, args);
+	}
+
+}
